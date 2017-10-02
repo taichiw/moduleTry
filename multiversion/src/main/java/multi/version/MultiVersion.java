f7a9ad00767f2be0bb4cfd22096529c5b3d08acb
Taichi Watanabe <taichi.watanabe@rakuten.com>
2017-10-02 21:22:59 +0900

    2になっちゃった！

---
+++ multiversion/src/main/java/multi/version/MultiVersion.java
@@ -0,0 +1,7 @@
+package multi.version;
+
+public class MultiVersion {
+	public static String version () {
+		return "2";
+	}
+}
--- org.astro/src/main/java/org/astro/World.java
+++ org.astro/src/main/java/org/astro/World.java
@@ -1,7 +1,8 @@
 package org.astro;
+import multi.version.MultiVersion;
 
 public class World {
 	public static String name() {
-		return "world";
+		return "world" + MultiVersion.version();
 	}
 }
